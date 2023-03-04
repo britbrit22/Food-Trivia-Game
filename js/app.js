a82aad4fe91fb4ca83fe1daefdff6a8a46a9f93d
britbrit22 <https://github.com/britbrit22/Hidden-Foods-Games.git>
2023-03-04 09:59:16 -0800

    add button

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//User clicks on the play button
+// -What would it take for the users to click on the button.
+const button = document.createElement('button')
+
+      button.innerText = 'Press Start'
+
+      button.id = 'BeginButton'
+
+    
+      button.addEventListener('click', () => {
+        
+        alert('Begin Game')
+      })
+
+      document.body.appendChild(button)
+
+//Remove the button and display the questions and answers.
+// -How woueld I remove the button then activate the questions and answers.
+
+
+
+//User selects an answer.
+// -What would it take for the users to click an answer.
+
+//Display to the user if the answer is correct or incorrect.
+//-How would I make the answer correct or incorrect.
