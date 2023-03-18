d7e875600504c61b3bbc91d18ceb0a4876c3b411
britbrit22 <https://github.com/britbrit22/Hidden-Foods-Games.git>
2023-03-17 23:36:14 -0700

    changes

--- js/app.js
+++ js/app.js
@@ -1,193 +1,138 @@
-//User clicks on the play button
-// -What would it take for the users to click on the button.
-const button = document.createElement('button')
+const startButton = document.getElementById('startButton')
+const nextButton = document.getElementById('nextButton')
+const questionContainer = document.querySelector('.question-container')
+const answerContainer = document.getElementById('answer-container')
+// const question = document.querySelector('.question')
+// const answers = document.getElementById("answer")
 
-      button.innerText = 'Press Start'
-
-      button.id = 'BeginButton'
-      document.body.append(button)
-    
-      button.addEventListener('click', click)
+// const questionElement = document.getElementById('question');
 
 
-     function click() {
-        console.log('Begin Game')
-      
 
-      button.removeEventListener('click', click)
-    }
+let shuffledQuestions, currentQuestionIndex
 
-    
 
-//Remove the button and display the questions and answers.
-// -How would I remove the button then activate the questions and answers.
+startButton.addEventListener('click', startTrivia)
+nextButton.addEventListener('click', () => {
+currentQuestionIndex++
+setNextQuestion(nextButton)
+})
 
-//const question = document.querySelector('question');
-//const answers = document.querySelectorAll('answers');
+function startTrivia() {
+   console.log('Started')
+   startButton.classList.add('hide')
+   shuffledQuestions = questions.sort(() => Math.random())
+   currentQuestionIndex = 0
+   // question.classList.remove('hide')
+   setNextQuestion()
+}
+
+function setNextQuestion() {
+   reset()
+   showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+   showQuestion.innerText = question.question
+   question.answer.forEach(answer => {
+   const button = document.createElement('button')
+   button.innerText = answer.text
+   button.classList.add('button')
+   if(answer.correct) {
+      button.dataset.correct = answer.correct
+   }
+      button.addEventListener('click', selectAnswer)
+      answerContainer.append(button)
+   })
+}
+
+function reset() {
+while(answerContainer.firstChild) {
+   answerContainer.removeChild(answerContainer.firstChild)
+}
+}
+
+
+ function selectAnswer(event) {
+   const selectedButton = event.target
+   const correct = selectedButton.dataset.correct
+   // setStatusClass(document.body, correct)
+   // Array.from(answerContainer.child).forEach(button => {
+   // setStatusClass(button, button.dataset.correct)
+   // })
+   if (shuffledQuestions.length > currentQuestionIndex + 1) {
+   nextButton.classList.remove('hide')
+   } else {
+     startButton.innerText = 'Restart'
+     startButton.classList.remove('hide')
+   }
+}
+
 
 const questions =  [
     {
         question: "Which fast-food restaurant has the slogan, Eat fresh?",
-        answer1: "Taco Bell", 
-        answer2: "Subway",
-        answer3: "Wendys",
-        answer4: "Arbys",
-        correctAnswer: "Subway",
+        answer: [
+        { id:1, text: "Taco Bell", correct: false },
+        { id:2, text: "Subway", correct: true },
+        { id:3, text: "Wendys", correct: false },
+        { id:4, text: "Arbys", correct: false },
+        ],
      },
      {
         question: "What is the most common pizza topping?",
-        answer1: "Pepperoni",
-        answer2: "Ham",
-        answer3: "Sausage",
-        answer4: "Bacon",
-        correctAnswer: "Pepperoni",
-     },
+        answer: [
+        { id:1, text: "Pepperoni", correct: true },
+        { id:2, text: "Ham", correct: false },
+        { id:3, text: "Sausage", correct: false },
+        { id:4, text: "Bacon", correct: false },
+        ],
+     },  
      {
         question: "How many flavors of ice cream does Baskin-Robbins have?",
-        answer1: "22",
-        answer2: "60",
-        answer3: "40",
-        answer4: "31",
-        correctAnswer: "31",
+        answer: [
+        { id:1, text: "22", correct: false },
+        { id:2, text: "60", correct: false },
+        { id:3, text: "40", correct: false },
+        { id:4, text: "31", correct: true },
+        ],
+        
      },   
      {  
         question: "Coca-Cola was first sold in which state?",
-        answer1: "New York",
-        answer2: "Arizona",
-        answer3: "Georgia",
-        answer4: "Texas",
-        correctAnswer: "Georgia",
+        answer: [
+        { id:1, text: "New York", correct: false },
+        { id:2, text: "Arizona", correct: false },
+        { id:3, text: "Georgia", correct: true },
+        { id:4, text: "Texas", correct: false },
+        ], 
      },
      {
         question: "What is the oldest soft drink in America?",
-        answer1: "Coca Cola",
-        answer2: "Pepsi",
-        answer3: "Sprite",
-        answer4: "Dr. Pepper",
-        correctAnswer: "Dr. Pepper",
+        answer: [
+        { id:1, text: "Coca Cola", correct: false },
+        { id:2, text: "Dr. Pepper", correct: true },
+        { id:3, text: "Sprite", correct: false },
+        { id:4, text: "Pepsi", correct: false },
+        ],
      }
 ]
+// const currentQuestion = questions[currentQuestionIndex];
+// question.innerText = currentQuestion.question;
 
- const start = true;
- console.log({start})
-
-
-const answer = document.querySelectorAll(".answer")
-
-console.log({answer})
-      
-const question = document.querySelector(".question");
-
-console.log({question})
-
-const next = document.querySelector(".next");
-console.log({next})
-
-// const next = document.querySelector(".next")
-// console.log({next})
+questions.innerHTML = questions.question
+// questionContainer.innerHTML = question.question
+// questions.innerHTML = questions[1].question
+// questions.innerHTML = questions[2].question
+// questions.innerHTML = questions[3].question
+// questions.innerHTML = questions[4].question
+// questions.innerHTML = questions[5].question
 
 
-
-const answer1 = document.querySelector('.answer1');
-console.log({answer1})
-const answer2 = document.querySelector('.answer2');
-console.log({answer2})
-const answer3 = document.querySelector('.answer3');
-console.log({answer3})
-const answer4 = document.querySelector('.answer4');
-console.log({answer4})
-
-
-question.innerText = questions[0].question;//Injecting the question onto the question div.
-answer1.innerText = questions[0].answer1;//Injecting the answer1 onto the answer1 div.
-answer2.innerText = questions[0].answer2;
-answer3.innerText = questions[0].answer3;
-answer4.innerText = questions[0].answer4;
-
-
-function checkAnswer(event) {
-   
-const choice = event.target.innerText
-   console.log(choice)
- if (choice === questions[0].correctAnswer) {
-    console.log(choice)
-
-let currentIndex = 0;
-
- if (currentIndex >= questions.length)  
- {
-   console.log('end of questions')
- } else { 
-   console.log(questions[currentIndex]);
- }
-
-
-
- 
-
-
-}
-}
-
-      //Increase the currentIndex by 1📝
-
-
-
- 
-   
-
-
-answer1.addEventListener("click", checkAnswer)   
-
-answer2.addEventListener("click", checkAnswer)
-  
-answer3.addEventListener("click", checkAnswer)
-   
-answer4.addEventListener("click", checkAnswer)
-
-
-
-
+// answer1.innerHTML = answer1
+// answer2.innerHTML = answer2
+// answer3.innerHTML = answer3
+// answer4.innerHTML = answer4
 
 
    
-const selected = "";
-
-answer2.addEventListener("click", () => {
-   answer1.style.backgroundColor = "red"
-   answer2.style.backgroundColor = "green"
-   answer3.style.backgroundColor = "red"
-   answer4.style.backgroundColor = "red"
-   selected = answer2.value;
-
-})
-
-
-
-// const choice = document.querySelector("check");
-
-//  checkAnswer[0].addEventListener("click"); 
-//    if(choice == "true") {
-//       answer[0].innerText = "True";
-//       answer.style.color = "green"; 
-//    } else {
-//        answer.innerText = "False";
-//       answer[0].style.color = "red";
-//    }
-    
-
-
-
-
-
-    
-
-
-//User selects an answer.
-// -What would it take for the users to click an answer.
-
-
-
-//Display to the user if the answer is correct or incorrect.
-//-How would I make the answer correct or incorrect.
-
